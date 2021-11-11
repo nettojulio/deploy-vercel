5eac4850f71960e866771719afda6d455987d6d5
Julio Neto <nettojulio@hotmail.com>
2021-11-10 22:14:34 -0300

    initial commit

---
+++ src/pages/EdicaoProduto/index.js
@@ -0,0 +1,96 @@
+import {
+  Backdrop, Button, CircularProgress, Divider, InputAdornment,
+  Snackbar, TextField, Typography
+} from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
+import useAuth from '../../hook/useAuth';
+import { put } from '../../services/ApiClient';
+import useStyles from './styles';
+
+
+function EdicaoProduto() {
+  const classes = useStyles();
+  const history = useHistory();
+  const { id, nome, imagem } = history.location.state ?? {};
+  const { register, handleSubmit } = useForm();
+  const { token } = useAuth();
+  const [erro, setErro] = useState('');
+  const [carregando, setCarregando] = useState(false);
+
+  async function onSubmit(data) {
+    try {
+      setCarregando(true);
+      setErro('');
+
+      const dadosAtualizados = Object.fromEntries(Object.entries(data).filter(([, value]) => value));
+
+      const { dados, erro } = await put(`produtos/${id}`, dadosAtualizados, token);
+
+      if (erro) {
+        setErro(dados);
+        return; 
+      }
+
+      history.push('/produtos');
+    } catch (error) {
+      setErro(error.message);
+    } finally {
+      setCarregando(false);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Typography variant="h4">Editar produto</Typography>
+      <div className={classes.container}>
+        <div className={classes.formContainer}>
+          <TextField label="Nome do produto" {...register('nome')} />
+          <div className="columns">
+          <TextField
+            label="Preço"
+            {...register('preco')}
+            InputProps={{
+              startAdornment: <InputAdornment position="start">R$</InputAdornment>,
+            }}
+          />
+          <TextField
+            label="Estoque"
+            {...register('estoque')}
+            InputProps={{
+              startAdornment: <InputAdornment position="start">Un</InputAdornment>,
+            }}
+          />
+          </div>
+          <TextField label="Descrição do produto" {...register('descricao')} />
+          <TextField label="Imagem" {...register('imagem')} />
+        </div>
+        <img src={imagem} alt={nome} />
+      </div>
+      <Divider className={classes.divider} />
+      <Link to="/produtos" className={classes.link}>CANCELAR</Link>
+      <Button 
+        className={classes.botao}
+        type="submit"
+      >SALVAR ALTERAÇÕES</Button>
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        autoHideDuration={3000}
+        open={!!erro}
+        key={erro}
+      >
+        <Alert onClose={() => setErro('')} severity="error">
+          {erro}
+        </Alert>
+      </Snackbar>
+      <Backdrop className={classes.backdrop} open={carregando}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </form>
+  )
+}
+
+export default EdicaoProduto;
---
+++ src/pages/NovoProduto/index.js
@@ -0,0 +1,93 @@
+import {
+  Backdrop, Button, CircularProgress, Divider, InputAdornment,
+  Snackbar, TextField, Typography
+} from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
+import useAuth from '../../hook/useAuth';
+import { post } from '../../services/ApiClient';
+import useStyles from './styles';
+
+function NovoProduto() {
+  const classes = useStyles();
+  const history = useHistory();
+  const { token } = useAuth();
+  const { register, handleSubmit } = useForm();
+  const [erro, setErro] = useState('');
+  const [carregando, setCarregando] = useState(false);
+
+  console.log("Test");
+
+  async function onSubmit(data) {
+    try {
+      setCarregando(true);
+      setErro('');
+
+      data.categoria = 'nao categorizado';
+
+      const { dados, erro } = await post('produtos', data, token);
+
+      if (erro) {
+        setErro(dados);
+        return;
+      }
+
+      history.push('/produtos');
+    } catch (error) {
+      setErro(error.message); 
+    } finally {
+      setCarregando(false);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Typography variant="h4">Adicionar produto</Typography>
+      <div className={classes.formContainer}>
+        <TextField label="Nome do produto" {...register('nome', { required: true })} />
+        <div className="columns">
+        <TextField
+          label="Preço"
+          {...register('preco', { required: true })}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">R$</InputAdornment>,
+          }}
+        />
+        <TextField
+          label="Estoque"
+          {...register('estoque', { required: true })}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">Un</InputAdornment>,
+          }}
+        />
+        </div>
+        <TextField label="Descrição do produto" {...register('descricao', { required: true })} />
+        <TextField label="Imagem" {...register('imagem')} />
+      </div>
+      <Divider className={classes.divider} />
+      <Link to="/produtos" className={classes.link}>CANCELAR</Link>
+      <Button 
+        className={classes.botao}
+        type="submit"
+      >ADICIONAR PRODUTO</Button>
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        autoHideDuration={3000}
+        open={!!erro}
+        key={erro}
+      >
+        <Alert onClose={() => setErro('')} severity="error">
+          {erro}
+        </Alert>
+      </Snackbar>
+      <Backdrop className={classes.backdrop} open={carregando}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </form>
+  )
+}
+
+export default NovoProduto;
